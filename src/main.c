19c232cc0aea41752e90c264560071cd7249effb
Ulisses Cardoso <uli.citrinitas@gmail.com>
2024-03-18 15:01:25 +0000

    tratamento dos argumentos

--- src/main.c
+++ src/main.c
@@ -5,5 +5,12 @@ int main(int argc, char **argv)
 {
     printf("%s was running with %d args\n", argv[0], argc-1);
 
+    if(argc <= 1){
+        puts("Erro! precisa ter ao menos 7 argumentos");
+        return EXIT_FAILURE;
+    }
+
+    
+
     return 0;
 }
