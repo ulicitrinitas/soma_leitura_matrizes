97056c4df4129359e8cc70d31544ee99705050d2
Ulisses Cardoso <uli.citrinitas@gmail.com>
2024-03-18 15:28:23 +0000

    leitura do arquivo

--- src/main.c
+++ src/main.c
@@ -10,19 +10,51 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    const int num_threads = atoi(argv[2]);
-    const int num_msize = atoi(argv[3]);
+    const int num_threads = atoi(argv[1]);
+    const int num_msize = atoi(argv[2]);
+
+    printf("num_threads: %d\nnum_msize: %d\n", num_threads, num_msize);
 
     FILE *f1;
     FILE *f2;
 
-    f1 = fopen(argv[4], "r");
-    f2 = fopen(argv[5], "r");
+    f1 = fopen(argv[3], "r");
+    f2 = fopen(argv[4], "r");
 
     if(f1 == NULL || f2 == NULL){
         puts("Erro! Leitura dos arquivos falhou!");
         return EXIT_FAILURE;
     }
+
+    printf("f1: %s\nf2: %s\n", argv[4], argv[5]);
+
+    char **linhas_m1 = (char**) calloc(num_msize, sizeof(char*));
+
+    if(linhas_m1 == NULL){
+        puts("Erro! Espaco insuficiente!");
+        return EXIT_FAILURE;
+    }
+
+    for(int i  = 0; i < num_msize; i++){
+        linhas_m1[i] = (char*) calloc(1024, sizeof(char));
+    }
+
+    if(linhas_m1[1] == NULL){
+        puts("Erro! Espaco insuficiente no vetor!");
+        return EXIT_FAILURE;
+    }
+
+    int count = 0;
+
+    while(!feof(f1)){
+        fgets(linhas_m1[count], 1024, f1);
+        count++;
+    }
+
+    for(int i = 0; i < num_msize; i++){
+        printf("%s", linhas_m1[i]);
+    }
+    puts("");
 
     
 
